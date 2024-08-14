01b1e24c3ec40ce68bc0e7ff4104c8704ff3e7db
Bensafyrajae <bensafyrajae19@gmail.com>
2024-08-14 14:49:21 +0100

    ajouter les 3 premiers section

--- public/js/app.js
+++ public/js/app.js
@@ -6,16 +6,16 @@ myButtons.forEach(btn => {
     btn.addEventListener("click", () => {
         let modalData = btn.getAttribute("modal-data")
         myModals.forEach(modal => {
-            // * show modal
+          
             if (modal.id == modalData) {
                 modal.style.display = "flex"
             }
-            // ! close modal with button
+          
             modal.querySelector(".close-modal").addEventListener("click", () => {
                 modal.style.display = "none"
             })
 
-            // ! close modal with backdrop press
+            
             modal.addEventListener("click", () => {
                 modal.style.display = "none"
             })
